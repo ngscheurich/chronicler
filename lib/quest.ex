4986e807fe4241f2852b4019a1653c3012c88313
N. G. Scheurich <nick@scheurich.me>
2022-11-16 17:54:15 -0600

    Allow quests to fail

--- lib/quest.ex
+++ lib/quest.ex
@@ -4,14 +4,16 @@ defmodule Chronicler.Quest do
 
   @frequency 1000
 
-  defstruct [:title, duration: 3000]
+  defstruct [:title, danger: 3, duration: 3000]
 
   def loop(%Quest{} = quest) do
     receive do
       :embark ->
         message(quest, :embark)
-        Process.sleep(quest.duration)
-        message(quest, :success)
+        tick(quest)
+
+      :tick ->
+        tick(quest)
 
       :inspect ->
         quest |> dbg() |> loop()
@@ -26,5 +28,33 @@ defmodule Chronicler.Quest do
   defp message(%{title: title}, :success) do
     msg = "🎉 Quest complete: “#{title}”"
     Messenger.out(msg)
+  end
+
+  defp message(%{title: title}, :failure) do
+    msg = "💀 Quest failed: “#{title}”"
+    Messenger.out(msg)
+  end
+
+  defp tick(%{duration: duration} = quest) do
+    tempt_fate(quest)
+
+    case duration - @frequency do
+      num when num <= 0 ->
+        message(quest, :success)
+        Process.exit(self(), :normal)
+
+      num ->
+        Process.send_after(self(), :tick, @frequency)
+        loop(%{quest | duration: num})
+    end
+  end
+
+  defp tempt_fate(quest) do
+    roll = Enum.random(1..20)
+
+    if roll + quest.danger >= 20 do
+      message(quest, :failure)
+      Process.exit(self(), :ill_fated)
+    end
   end
 end
